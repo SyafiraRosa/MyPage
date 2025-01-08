f6e5dad975825d552a3d8d9f4d07562a3b984597
SyafiraRosa <firarosa966@gmail.com>
2025-01-08 18:06:23 +0700

    update next config & page logo

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'export',
-    // basePath: '/MyPage', // Adjust to match your GitHub Pages repository name
-    // assetPrefix: '/MyPage', // Ensure asset paths are correct
-    
-};
+const nextConfig = {};
 
 export default nextConfig;
